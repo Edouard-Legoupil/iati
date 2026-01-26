ecb9fb1a292a0b423df4bc437b4eb541e97b653c
Edouard Legoupil <edouard.legoupil@gmail.com>
2026-01-26 14:07:14 +0100

    WIP

--- inst/source/requirements.R
+++
@@ -1,9 +0,0 @@
-if (!require("pacman")) install.packages("pacman")
-if (!require("here")) install.packages("here")
-
-pacman::p_load(tidyverse,
-               xml2,
-               svMisc,
-               fs,
-               furrr,
-               writexl)
